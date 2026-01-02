1b1880b9951847e56ea9cbc9be3a3d0fb827c1b5
AyeshaMazhar-AM <ayeshamazhar332@gmail.com>
2026-01-02 18:58:47 +0500

    code efficiency

--- challenge1_product_array.py
+++ challenge1_product_array.py
@@ -1,19 +1,3 @@
-# def team_impact(contributions):
-#     ln = len(contributions)
-    
-#     left = [1] * ln
-#     right = [1] * ln
-#     impact = [1] * ln
-
-#     for i in range(1, ln):
-#         left[i] = left[i - 1] * contributions[i - 1]
-#     for i in range(ln - 2, -1, -1):
-#         right[i] = right[i + 1] * contributions[i + 1]
-#     for i in range(ln):
-#         impact[i] = left[i] * right[i]
-        
-#     return impact
-
 def team_impact(contributions):
     ln = len(contributions)
 
